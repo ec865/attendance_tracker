1c983b26d68e289530fbefcaad279c8239234db0
Bora Ozsoy <boraozsoy@gmail.com>
2022-01-06 19:09:53 +0000

    updated the api

--- server/src/app.py
+++ server/src/app.py
@@ -20,7 +20,7 @@ db = firestore.client()
 
 
 def add_user(name, surname, email, password, role):
-    user = db.collection(u'users').document(name+surname)
+    user = db.collection(u'users').document(name + '_' + surname)
     user.set({
         u'name': name,
         u'surname': surname,
@@ -37,12 +37,8 @@ def list_users():
         users[user.id] = user.to_dict()
     return json.loads(json.dumps(users))
 
-def set_current_user(user_id):
-    global current_user 
-    current_user = user_id
-
 def add_event(name, user_id):
-    event = db.collection(u'events').document(name)
+    event = db.collection(u'events').document(name + '_' + user_id)
     event.set({
         u'event_name': name,
         u'user_id': user_id,
@@ -59,12 +55,14 @@ def list_events():
 def delete_event(event):
     db.collection(u'events').document(event).delete()
 
-def add_description(des_name, passcode, status, event_id):
-    des = db.collection(u'descriptions').document(des_name)
+def add_description(des_name, passcode, status, start_time, end_time, event_id):
+    des = db.collection(u'description').document(event_id + '_' + des_name)
     des.set({
         u'description_name': des_name,
         u'passcode': passcode,
         u'status' : status,
+        u'start_time': start_time,
+        u'end_time': end_time,
         u'event_id' : event_id
     })        
 
@@ -76,11 +74,11 @@ def list_descriptions():
         des_of_event[des.id] = des.to_dict()
     return json.loads(json.dumps(des_of_event))
 
-def add_attendance(start_time, end_time):
-    att = db.collection(u'descriptions').document()
+def add_attendance(user_id, description_id):
+    att = db.collection(u'attendance').document(user_id + '_' + description_id)
     att.set({
-        u'start_time': start_time,
-        u'end_time': end_time
+        u'user_id': user_id,
+        u'description_id': description_id
     })      
 
 def list_attendances():
@@ -95,9 +93,7 @@ def list_attendances():
 app = Flask(__name__)
 CORS(app)
 
-def format_server_time():
-    server_time = time.localtime()
-    return time.strftime("%I:%M:%S %p", server_time)
+######################### API ############################
 
 @app.route('/', methods=['GET', 'POST'])
 @cross_origin()
@@ -135,9 +131,14 @@ def sign_up():
     password = request.args.get('password')
     role = request.args.get('role')
     add_user(name, surname, email, password, role)
-    return "user " + name + " added"
+    return "User " + name + " added"
 
-@app.route('/api/events/<string:user_id>', methods=['GET', 'POST'])
+@app.route('/api/events', methods=['GET'])
+@cross_origin()
+def l_events():
+    return list_events()
+
+@app.route('/api/events/<string:user_id>', methods=['GET'])
 @cross_origin()
 def events(user_id):
     #current_user = request.args.get('user_id')
@@ -147,19 +148,74 @@ def events(user_id):
         events_of_user[event.id] = event.to_dict()
     return json.dumps(events_of_user)
 
-@app.route('/api/descriptions', methods=['GET', 'POST'])
+@app.route('/api/events/<string:user_id>/add', methods=['POST'])
 @cross_origin()
-def descriptions():
-    event_id = request.args.get('event_id')
+def a_event(user_id):
+    event_name = request.args.get('event_name')
+    return "event added" , add_event(event_name, user_id)
+
+
+@app.route('/api/descriptions', methods=['GET'])
+@cross_origin()
+def l_descriptions():
+    return list_descriptions()
+
+@app.route('/api/descriptions/<string:event_id>', methods=['GET'])
+@cross_origin()
+def descriptions(event_id):
     docs = db.collection(u'description').where(u'event_id', u'==', event_id).stream()
     des_of_event = {}
     for des in docs:
         des_of_event[des.id] = des.to_dict()
+    return json.dumps(des_of_event)
+
+@app.route('/api/descriptions/<string:event_id>/add', methods=['POST'])
+@cross_origin()
+def a_description(event_id):
     des_name = request.args.get('des_name')
     passcode = request.args.get('passcode')
     status = request.args.get('status')
-    add_description(des_name, passcode, status, event_id)
-    return json.dumps(des_of_event)
+    start_time = request.args.get('start_time')
+    end_time = request.args.get('end_time')
+    return "description added" , add_description(des_name, passcode, status, start_time, end_time, event_id)
+
+
+@app.route('/api/attendances', methods=['GET'])
+@cross_origin()
+def l_attedances():
+    return list_attendances()
+
+@app.route('/api/attendances/u/<string:user_id>', methods=['GET'])
+@cross_origin()
+def attedances_by_user(user_id):
+    docs = db.collection(u'attendance').where(u'user_id', u'==', user_id).stream()
+    atts = {}
+    for att in docs:
+        atts[att.id] = att.to_dict()
+    return json.dumps(atts)
+
+@app.route('/api/attendances/d/<string:des_id>', methods=['GET'])
+@cross_origin()
+def attedances_by_description(des_id):
+    docs = db.collection(u'attendance').where(u'description_id', u'==', des_id).stream()
+    atts = {}
+    for att in docs:
+        atts[att.id] = att.to_dict()
+    return json.dumps(atts)
+
+@app.route('/api/attendances/<string:user_id>/<string:des_id>', methods=['GET'])
+@cross_origin()
+def attedances_by_user_description(user_id, des_id):
+    docs = db.collection(u'attendance').where(u'user_id', u'==', user_id).where(u'description_id', u'==', des_id).stream()
+    atts = {}
+    for att in docs:
+        atts[att.id] = att.to_dict()
+    return json.dumps(atts)
+
+@app.route('/api/attendances/<string:user_id>/<string:des_id>/add', methods=['POST'])
+@cross_origin()
+def a_attedances(user_id, des_id):
+    return "description added" , add_attendance(user_id, des_id)
 
 
 if __name__ == '__main__':
